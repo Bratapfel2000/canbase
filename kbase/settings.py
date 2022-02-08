1717962f0f8f4b143f6f7a3dd7a06160816a8acd
patrickhaetti <patrickhaetti@gmx.de>
2022-02-08 12:36:15 +0100

    debug=true

--- kbase/settings.py
+++ kbase/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 #SECRET_KEY = 'test'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['canbase.herokuapp.com']
 

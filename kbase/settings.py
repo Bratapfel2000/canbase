ec580822e6f871dd1fd5ceb9be92d4f11b2692cf
patrickhaetti <patrickhaetti@gmx.de>
2022-02-09 17:12:28 +0100

    add restframeork to settings, add django startapp api and add api to settings

--- kbase/settings.py
+++ kbase/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'storages'
+    'storages',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
